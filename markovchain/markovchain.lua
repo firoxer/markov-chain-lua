0811439403286574408b0d5ee65bc9159f91a657
Oliver Vartiainen <oliver@iki.fi>
2017-05-08 22:43:17 +0300

    Initial commit

---
+++ markovchain/markovchain.lua
@@ -0,0 +1,51 @@
+local Chain = {}
+
+function Chain:link(a, b)
+	if self.links[a] == nil then
+		self.links[a] = {}
+	end
+
+	if self.links[a][b] == nil then
+		self.links[a][b] = 0
+	end
+
+	self.links[a][b] = self.links[a][b] + 1
+end
+
+function Chain:pickOneAfter(sourceItem)
+	if self.links[sourceItem] == nil then
+		return nil
+	end
+
+	local totalItemCount = 0
+	for _, itemCount in pairs(self.links[sourceItem]) do
+		totalItemCount = totalItemCount + itemCount
+	end
+
+	local pickedItemPosition = math.random(totalItemCount)
+
+	local currentPosition = 0
+	for item, itemCount in pairs(self.links[sourceItem]) do
+		currentPosition = currentPosition + itemCount
+
+		if currentPosition >= pickedItemPosition then
+			return item
+		end
+	end
+
+	return nil
+end
+
+local function newChain()
+	local self = {
+		links = {},
+	}
+
+	setmetatable(self, {__index = Chain})
+
+	return self
+end
+
+return {
+	new = newChain
+}
