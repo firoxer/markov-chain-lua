f3fa7c0f1bd0da1f33ef28003ed764610c2db16e
Oliver Vartiainen <oliver@iki.fi>
2017-06-27 20:27:38 +0300

    Refactor

--- markovchain/markovchain.lua
+++ markovchain/markovchain.lua
@@ -1,51 +1,40 @@
 local Chain = {}
 
 function Chain:link(a, b)
-	if self.links[a] == nil then
-		self.links[a] = {}
-	end
+   if self.links[a] == nil then self.links[a] = {} end
+   if self.links[a][b] == nil then self.links[a][b] = 0 end
 
-	if self.links[a][b] == nil then
-		self.links[a][b] = 0
-	end
-
-	self.links[a][b] = self.links[a][b] + 1
+   self.links[a][b] = self.links[a][b] + 1
 end
 
 function Chain:pickOneAfter(sourceItem)
-	if self.links[sourceItem] == nil then
-		return nil
-	end
+   if self.links[sourceItem] == nil then return nil end
 
-	local totalItemCount = 0
-	for _, itemCount in pairs(self.links[sourceItem]) do
-		totalItemCount = totalItemCount + itemCount
-	end
+   local totalItemCount = 0
+   for _, itemCount in pairs(self.links[sourceItem]) do
+      totalItemCount = totalItemCount + itemCount
+   end
 
-	local pickedItemPosition = math.random(totalItemCount)
+   local pickedItemPosition = math.random(totalItemCount)
 
-	local currentPosition = 0
-	for item, itemCount in pairs(self.links[sourceItem]) do
-		currentPosition = currentPosition + itemCount
+   local currentPosition = 0
+   for item, itemCount in pairs(self.links[sourceItem]) do
+      currentPosition = currentPosition + itemCount
 
-		if currentPosition >= pickedItemPosition then
-			return item
-		end
-	end
+      if currentPosition >= pickedItemPosition then return item end
+   end
 
-	return nil
+   return nil
 end
 
-local function newChain()
-	local self = {
-		links = {},
-	}
+local function new()
+   local self = {
+      links = {},
+   }
 
-	setmetatable(self, {__index = Chain})
+   setmetatable(self, {__index = Chain})
 
-	return self
+   return self
 end
 
-return {
-	new = newChain
-}
+return { new = new }
